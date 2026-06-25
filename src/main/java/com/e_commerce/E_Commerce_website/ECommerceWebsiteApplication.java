a96c6ac95e68fc89afab0b7c21b8ad33e8c8b78d
Ranaveer Gundaram <ranaveer.gundaram@kumaran.com>
2026-06-25 16:58:29 +0530

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/e_commerce/E_Commerce_website/ECommerceWebsiteApplication.java
@@ -0,0 +1,15 @@
+package com.e_commerce.E_Commerce_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebsiteApplication.class, args);
+		System.out.println("Spring Boot Application is successfully running....");
+
+	}
+
+}
